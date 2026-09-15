1add0ebede33973aa01a89c5b7b49d67bd620158
samu-kil <squinter2@eafit.edu.co>
2026-09-14 20:43:57 -0500

    Add files via upload

---
+++ proyecto04/HDL/Mult.asm
@@ -0,0 +1,53 @@
+@R2
+M=0         // RAM[2] = 0
+
+@R0
+D=M
+@i
+M=D         // i = RAM[0]
+
+@LOOP_NEG
+D;JLT
+@LOOP_POS
+0;JMP
+
+(LOOP_POS)
+
+    @i
+    D=M
+    @END
+    D;JEQ      // if i == 0 then goto END
+
+    @R1
+    D=M
+    @R2
+    M=D+M       // RAM[2] = RAM[2] + RAM[1]
+
+    @i
+    M=M-1       // i--
+    
+    @LOOP_POS
+    0;JMP
+
+
+(LOOP_NEG)
+
+    @i
+    D=M
+    @END
+    D;JEQ      // if i == 0 then goto END
+
+    @R1
+    D=-M        
+    @R2
+    M=D+M       // RAM[2] = RAM[2] + (-RAM[1])
+
+    @i
+    M=M+1       // i++
+    
+    @LOOP_NEG
+    0;JMP
+
+(END)
+    @END
+    0;JMP
